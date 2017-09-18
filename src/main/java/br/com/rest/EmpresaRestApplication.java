320be550798e8702ca8ffb4f7791bb4f76a4a536
Lucas Rodrigues <lucasrest@hotmail.com>
2017-09-18 19:57:58 -0300

    informações iniciais da api

---
+++ src/main/java/br/com/rest/EmpresaRestApplication.java
@@ -0,0 +1,12 @@
+package br.com.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpresaRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpresaRestApplication.class, args);
+	}
+}
